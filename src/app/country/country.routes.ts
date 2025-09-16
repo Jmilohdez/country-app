8c3263dcbd7a9bfda0389abf4b442a1ffc02832f
Juan Hernandez <camilohdez1017@gmail.com>
2025-09-16 09:34:30 -0500

    Layout Components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,23 +1,24 @@
-import { Routes } from "@angular/router";
-import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 
 export const countryRoutes: Routes = [
-
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+
+    ],
   },
-
-  // {
-  //   path: 'country',
-  //   // ??? {}
-  // },
-
-  // {
-  //   path: '**',
-  //   redirectTo: ''
-  // }
-
 ];
 
 export default countryRoutes;
